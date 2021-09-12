983df226f19b5ef79e5493aca8875dfb9c705723
Tasnim <87206003+mis455@users.noreply.github.com>
2021-09-13 01:05:20 +0600

    Add files via upload

---
+++ Final MIS 455 java/Final Exam.js
@@ -0,0 +1,35 @@
+
+
+// ES6 versionvar name = "someone";
+    var number1= 20;
+    var number2= 23.45; of JS
+
+let DISCOUNT = 20 % (total, quantity);
+
+
+
+let payable = total - DISCOUNT;
+
+
+
+
+document.getElementById("discount").innerText = discount;
+
+document.getElementById("payable").innerText = payable;
+let age = 22;
+let week;
+
+
+ if (age >= 50){
+   console.log (person, your age is above 50);
+else {
+
+    if(age <=50){
+    
+    console.log (your age is below 50)
+}
+//console.log("discount amount:", DISCOUNT);
+
+
+
+//console.log("Payable amount:", payable);
